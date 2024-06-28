7e075168c775c315d94f5d14a152ff51ea499a53
hakyy <rusic9990@gmail.com>
2024-06-28 15:19:01 +0200

    New things

---
+++ slide.js
@@ -0,0 +1,9 @@
+// import Splide from '@splidejs/splide';
+
+
+// // new Splide('.splide').mount();
+// new Splide( '.splide', {
+// type : 'loop',
+// perPage: 3,
+// });
+const mySlider = new Splide('#mySlider').mount();
